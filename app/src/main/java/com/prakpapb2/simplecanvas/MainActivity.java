26f0fba77b489f5f81c9025848b31a08d9f539ba
risa06 <risamahardika89@gmail.com>
2021-04-27 23:16:27 +0700

    second commit

--- app/src/main/java/com/prakpapb2/simplecanvas/MainActivity.java
+++ app/src/main/java/com/prakpapb2/simplecanvas/MainActivity.java
@@ -1,14 +1,37 @@
 package com.prakpapb2.simplecanvas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Canvas mCanvas;
+    private Paint mPaint = new Paint();
+    private Paint mPaintText = new Paint(Paint.UNDERLINE_TEXT_FLAG);
+    private Bitmap mBitmap;
+    private ImageView mimageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mimageView = findViewById(R.id.my_imageview);
+        mPaint.setColor(Color.YELLOW);
+        mPaintText.setColor(ResourcesCompat.getColor(getResources(),
+                R.color.purple_200, null));
+        mPaintText.setTextSize(60);
+    }
+
+    public void drawSomething(View view) {
+        int vWidth = view.getWidth();
+        int vHeight = view.getHeight();
+
     }
 }
