fea0d577456b18fba4f80d17ede8eccc8541b16f
risa06 <risamahardika89@gmail.com>
2021-04-27 22:27:10 +0700

    Initial commit

---
+++ app/src/main/java/com/prakpapb2/simplecanvas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prakpapb2.simplecanvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
